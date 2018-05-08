315dfde8430c65dee052bde87ecd75fe641134fc
David Banks <BanksySan@googlemail.com>
2018-05-08 10:29:03 +0100

    Threading.

---
+++ code-samples/memory-management/viewing-the-heap.cs
@@ -0,0 +1,12 @@
+namespace ViewingTheHeap
+{
+    using static System.Console;
+
+    internal static class Program
+    {
+        private static void Main()
+        {
+            WriteLine("Viewing the Heap");
+        }
+    }
+}
